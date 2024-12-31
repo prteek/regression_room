454d1db208b104079d0156f8ace256f2dd895d75
prteek <prteek@icloud.com>
2024-12-31 01:53:05 +0000

    renamed date to revision_date

--- posts/price_revision/data_cleanup.R
+++ posts/price_revision/data_cleanup.R
@@ -5,12 +5,12 @@ library(patchwork)
 library(this.path)
 setwd(here())
 # %%
-listings <- read_csv("listings.csv", show_col_types = F)
-revisions <- read_csv("revisions.csv", show_col_types = F)
+# We remove periods of COVID which may have caused market movement that are not representative of normal conditions
+min_data_date <- as.Date("2021-01-01")
+listings <- read_csv("listings.csv", show_col_types = F) %>%
+    filter(listing_date >= min_data_date)
+revisions <- read_csv("revisions.csv", show_col_types = F) %>%
+    filter(revision_date >= min_data_date)
 
-analysis_start_date <- as.Date("2021-01-01")
-analysis_end_date <- as.Date("2022-06-01")
-listings <- listings %>% filter(between(listing_date, analysis_start_date, analysis_end_date))
-revisions <- revisions %>% filter(between(price_revision_date, analysis_start_date, analysis_end_date))
 
 # %%
