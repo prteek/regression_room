c4bec9fad0b35f211b1bf9ca4ed79b5b4886878e
prteek <prteek@icloud.com>
2025-12-03 07:41:01 +0000

    formatting updates

--- posts/user_behaviour_analysis/prepare_states.r
+++ posts/user_behaviour_analysis/prepare_states.r
@@ -1,7 +1,7 @@
 # %%
 library(tidyverse)
 
-setwd("/Users/prateek/Documents/projects/prteek/regression_room/posts/user_behaviour_analysis")
+setwd(this.path::here())
 
 # %%
 dau_data <- read_csv("dau_data.csv", show_col_types = FALSE)
@@ -10,9 +10,13 @@ start_date <- min(dau_data$date)
 end_date <- max(dau_data$date)
 
 # %%
-full_dates <- tibble(expand.grid(date = seq(start_date, end_date, by = "day"), user_id = unique(dau_data$user_id)))
+full_dates <- tibble(expand.grid(
+    date = seq(start_date, end_date, by = "day"),
+    user_id = unique(dau_data$user_id)
+))
 
-all_active_days <- left_join(full_dates,
+all_active_days <- left_join(
+    full_dates,
     dau_data %>%
         mutate(is_active = TRUE, last_active_date = date),
     by = c("date", "user_id")
@@ -43,15 +47,25 @@ states <- filtered_active_days %>%
     fill(last_active_date, .direction = "down") %>%
     mutate(last_active_date = coalesce(lag(last_active_date), date)) %>%
     ungroup() %>%
-    mutate(state = case_when(
-        date == registration_date ~ "new",
-        (date != registration_date) & (is_active == TRUE) & (as.numeric(date - last_active_date) <= 6) ~ "current",
-        (is_active == TRUE) & (as.numeric(date - last_active_date) <= 30) & (as.numeric(date - last_active_date) > 6) ~ "reactivated",
-        (is_active == TRUE) & (as.numeric(date - last_active_date) > 30) ~ "resurrected",
-        (is_active == FALSE) & (as.numeric(date - last_active_date) <= 6) ~ "at_risk_wau",
-        (is_active == FALSE) & (as.numeric(date - last_active_date) <= 30) & (as.numeric(date - last_active_date) > 6) ~ "at_risk_mau",
-        TRUE ~ "dormant"
-    )) %>%
+    mutate(
+        state = case_when(
+            date == registration_date ~ "new",
+            (date != registration_date) &
+                (is_active == TRUE) &
+                (as.numeric(date - last_active_date) <= 6) ~ "current",
+            (is_active == TRUE) &
+                (as.numeric(date - last_active_date) <= 30) &
+                (as.numeric(date - last_active_date) > 6) ~ "reactivated",
+            (is_active == TRUE) &
+                (as.numeric(date - last_active_date) > 30) ~ "resurrected",
+            (is_active == FALSE) &
+                (as.numeric(date - last_active_date) <= 6) ~ "at_risk_wau",
+            (is_active == FALSE) &
+                (as.numeric(date - last_active_date) <= 30) &
+                (as.numeric(date - last_active_date) > 6) ~ "at_risk_mau",
+            TRUE ~ "dormant"
+        )
+    ) %>%
     dplyr::select(user_id, date, is_active, state, registration_date)
 
 write.csv(states, "states.csv", row.names = FALSE)
