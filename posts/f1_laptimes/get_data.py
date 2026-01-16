7a81e2873471acaedb5766d23293203420f02516
prteek <prteek@icloud.com>
2026-01-16 19:21:05 +0000

    getting sample lap data

--- posts/f1_laptimes/get_data.py
+++ posts/f1_laptimes/get_data.py
@@ -2,8 +2,27 @@
 import fastf1
 import pandas as pd
 from plotnine import *
+import duckdb
+import os
 
 #%%
-session = fastf1.get_session(2021, 7, 'Q')
+schedule = fastf1.get_event_schedule(2025)
+print(schedule.get(['RoundNumber', 'EventDate', 'Location', 'OfficialEventName']))
+
+#%%
+session = fastf1.get_session(2025, 7, 'R')
+session.load()
+
+print(session.event['EventName'])
+print(session.event['EventDate'])
+#%%
+laps_data = session.laps
+ver_nor = (laps_data
+            .query("Driver in ('VER', 'NOR')")
+            .assign(LapTime = lambda x: x['LapTime'].dt.total_seconds())
+            .get(['Driver', 'LapNumber', 'LapTime'])
+        )
+
+ver_nor.to_csv(os.path.join('posts', 'f1_laptimes' , 'data', 'sample_laptimes.csv'), index=False)
 
 #%%
