04951659e62a17531d7657dbc37745fb57353f1f
prteek <prteek@icloud.com>
2026-01-18 14:47:42 +0000

    Aggregate session laps across 2025 schedule (skip testing); remove VER/NOR filter, CSV export, and debug prints

--- posts/f1_laptimes/get_data.py
+++ posts/f1_laptimes/get_data.py
@@ -8,20 +8,14 @@ import os
 schedule = fastf1.get_event_schedule(2025)
 print(schedule.get(['RoundNumber', 'EventDate', 'Location', 'OfficialEventName']))
 
-#%%
-session = fastf1.get_session(2025, 1, 'R')
-session.load()
-
-print(session.event['EventName'])
-print(session.event['EventDate'])
-#%%
-laps_data = session.laps
-ver_nor = (laps_data
-            .query("Driver in ('VER', 'NOR')")
-            .assign(LapTime = lambda x: x['LapTime'].dt.total_seconds())
-            .get(['Driver', 'LapNumber', 'LapTime'])
-        )
-
-ver_nor.to_csv(os.path.join('posts', 'f1_laptimes' , 'data', 'sample_laptimes.csv'), index=False)
+race_laps = pd.DataFrame()
+for i, event in schedule.iterrows():
+  if event['EventFormat'] == 'testing':
+    continue
+  else:
+    event_round = event['RoundNumber']
+    session = fastf1.get_session(2025, 1, 'R')
+    session.load()
+    race_laps = pd.concat([race_laps, session.laps])
 
 #%%
